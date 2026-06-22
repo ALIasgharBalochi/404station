366dd8ad4b70b28e3d89bbe9b09fb5fbc181b57d
zizisoufi <z.soofi.n@gmail.com>
2026-06-22 09:25:17 -0700

    add database class

---
+++ database/database.py
@@ -0,0 +1,12 @@
+class DataBace:
+    def __init__(self):
+        self.data = []
+        
+    def add_data(self, data):
+        self.data.append(data)
+        
+    def update_data(self, data): #new_data
+        pass
+    
+    def remove_data(self, data)
+        
