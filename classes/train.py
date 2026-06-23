b96b83ec2d23f39d87c8043318fd4e5b87b7e5c8
ali_balochi <alibalochi1910@gmail.com>
2026-06-23 18:56:10 +0330

    impliment attributes of train class

---
+++ classes/train.py
@@ -0,0 +1,10 @@
+class Train:
+    def __init__(self,name,line,avarage_speed,quality,ticket_cost,capacity):
+        self.id = 1
+        self.name = name
+        self.line = line
+        self.avarage_speed = avarage_speed
+        self.quality = quality
+        self.ticket_cost= ticket_cost
+        self.capacity = capacity
+
