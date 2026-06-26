139f82a4568b19258ab1a53aa2962e0264fe431e
ali_balochi <alibalochi1910@gmail.com>
2026-06-27 00:44:17 +0330

    add optional checking for check

--- utilitys/checker.py
+++ utilitys/checker.py
@@ -4,14 +4,24 @@ email_pattern = r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
 password_pattern = r'^(?=.*[a-zA-Z])(?=.*\d)(?=.*@)(?=.*&)[a-zA-Z\d@&]{8,}$'
 
 
-def check(password,email):
+def check(password="",email="",options="b"):
     check_email = re.fullmatch(email_pattern,email)
     check_password = re.fullmatch(password_pattern,password)
-
-    if check_email and check_password:
-        return True
+    if options == "e":
+        if check_email :
+            return True
+        else:
+            return False
+    elif options == "p":
+        if  check_password:
+            return True
+        else:
+            return False
     else:
-        return False
+        if check_email and check_password:
+            return True
+        else:
+            return False
     
     
 
